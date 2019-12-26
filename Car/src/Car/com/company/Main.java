dbd282d6543632f2884e1bacd7f8f777318c79e4
Maks Bazyuk <mysik.bazyk@gmail.com>
2019-12-26 20:32:43 +0200

    Car

---
+++ Car/src/Car/com/company/Main.java
@@ -0,0 +1,12 @@
+package Car.com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        car.distanse();
+        car.grive(499);
+        car.reFuel(234);
+       }
+}
