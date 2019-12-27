2b61c8edf2185b4acaf7c16cc14135be6a0b56aa
Volodymyr00 <istoria1918@gmail.com>
2019-12-27 15:21:55 +0200

    Added Class Car

---
+++ com/company/Car.java
@@ -0,0 +1,35 @@
+package company;
+
+public class Car {
+    public double fuelconsumption;
+    public double volumeoffuel;
+    public double allkm;
+    public String color;
+    public String brand;
+    public double distance;
+    public int km;
+
+    public Car(String color, String brand, int fuelconsumption, int volumeoffuel, int allkm){
+        this.color = color;
+        this.brand = brand;
+        this.fuelconsumption = fuelconsumption;
+        this.volumeoffuel = volumeoffuel;
+        this.allkm = allkm;
+    }
+    public void drive(int km) {
+        distance = 100/ this.fuelconsumption * this.volumeoffuel;
+        System.out.println("Відстань, яку проїхав автомобіль: " + distance);
+        System.out.println("Загальний пробіг: " + (allkm + distance));
+    }
+
+    public void reFuel(int volumeoffuel) {
+        if (distance >= km){
+            System.out.println("Бензин зікінчився - потрібно заправити авто.");
+        }
+    }
+
+    public void distance1() {
+        System.out.println(this.color + " " + this.brand + ", пробіг: " + (allkm+distance));
+    }
+
+}
