96439b0c41f23f6cb9ff91c9fc608c517c5c2f7a
Volodymyr00 <istoria1918@gmail.com>
2020-01-19 15:40:03 +0200

    Delete README.md

--- com/company/Car.java
+++ com/company/Car.java
@@ -10,27 +10,28 @@ public class Car {
     public double volumeoffuel;
     public double allkm;
 
-    public Car(String color, String brand, int fuelconsumption, int volumeoffuel, int allkm){
+    public Car(String color, String brand, int fuelconsumption, int volumeoffuel, int allkm) {
         this.color = color;
         this.brand = brand;
         this.fuelconsumption = fuelconsumption;
         this.volumeoffuel = volumeoffuel;
         this.allkm = allkm;
     }
+
     public void drive(int km) {
-        distance = 100/ this.fuelconsumption * this.volumeoffuel;
+        distance = 100 / this.fuelconsumption * this.volumeoffuel;
         System.out.println("Відстань, яку проїхав автомобіль: " + distance);
         System.out.println("Загальний пробіг: " + (allkm + distance));
     }
 
     public void reFuel(int volumeoffuel) {
-        if (distance >= km){
+        if (distance >= km) {
             System.out.println("Бензин зікінчився - потрібно заправити авто.");
         }
     }
 
     public void distance1() {
-        System.out.println(this.color + " " + this.brand + ", пробіг: " + (allkm+distance));
+        System.out.println(this.color + " " + this.brand + ", пробіг: " + (allkm + distance));
     }
 
 }
--- com/company/Main.java
+++ com/company/Main.java
@@ -1,11 +1,11 @@
 package company;
 
 public class Main {
-        public static void main (String[]args){
-            Car myCar = new Car("Red", "Mazda", 10, 70, 10000);
-            myCar.drive(100);
-            myCar.reFuel(20);
-            myCar.distance1();
+    public static void main(String[] args) {
+        Car myCar = new Car("Red", "Mazda", 10, 70, 10000);
+        myCar.drive(100);
+        myCar.reFuel(20);
+        myCar.distance1();
 
-        }
     }
+}
